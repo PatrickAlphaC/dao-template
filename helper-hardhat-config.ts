5855e2d3e03b64080c49d31a84bda5fe894fd176
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-11 18:11:24 +0500

    Update helper-hardhat-config.ts file - Sepolia Update
    Replace goerli object with sepolia

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -10,7 +10,7 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
   localhost: {},
   hardhat: {},
-  goerli: {
+  sepolia: {
     blockConfirmations: 6,
   },
 }
